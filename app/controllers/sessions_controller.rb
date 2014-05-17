b703b93c9765a087f555c8c69ecfb8c047ac9f69
Michał Połtyn <mike@poltyn.com>
2014-05-17 15:16:26 +0100

    try to un-smell sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,27 +7,26 @@ class SessionsController < ApplicationController
   # POST Receives the oauth code from SalesKing, saves it to session
   # renders canvas haml
   def create
-    r = SK::SDK::SignedRequest.new(params[:signed_request], Sk::App.secret)
-    raise "invalid SalesKing app signed-request #{r.data.inspect}" unless r.valid?
+    signed_request = SK::SDK::SignedRequest.new(params[:signed_request], Sk::App.secret)
+    raise "Invalid SalesKing app signed request #{signed_request.data.inspect}" unless signed_request.valid?
     # always save and set subdomain
-    Sk::App.sub_domain = session['sub_domain'] = r.data['sub_domain']
-    if r.data['user_id'] # logged in
-      # new session with access_token, user_id, sub_domain
-      session['access_token'] = r.data['access_token']
-      session['user_id'] = r.data['user_id']
-      session['company_id'] = r.data['company_id']
-      session['language'] = find_locale(r.data['language'])
+    Sk::App.sub_domain = session['sub_domain'] = signed_request.data['sub_domain']
+    if signed_request.data['user_id'] # logged in
+      allowed_keys = %w(access_token user_id company_id)
+      session.merge!(signed_request.data.reject {|key,_| !allowed_keys.include?(key) })
+      session['language'] =  find_locale(signed_request.data['language'])
       redirect_to attachments_url
     else # must authorize redirect to oauth dialog
-        render inline: "<script> top.location.href='#{Sk::App.auth_dialog}'</script>"
+      render inline: "<script> top.location.href='#{Sk::App.auth_dialog}'</script>"
     end
   end
 
   #GET coming back from auth dialog as redirect_url
   def new
-    if params[:code] # coming back from auth dialog as redirect_url
+    code = params[:code]
+    if code.present? # coming back from auth dialog as redirect_url
       Sk::App.sub_domain = session['sub_domain']
-      Sk::App.get_token(params[:code])
+      Sk::App.get_token(code)
       #redirect to sk internal canvas page, where we are now authenticated
       render inline: "<script> top.location.href='#{Sk::App.sk_canvas_url}'</script>"
     end
@@ -36,7 +35,6 @@ class SessionsController < ApplicationController
   private
 
   def find_locale(locale)
-    posix_match = locale.match(/\A(?<country>[a-z]{2})_[A-Z]{2}\Z/)
-    posix_match.present? ? posix_match[:country] : locale
+    locale.try(:match, /\A(?<country>[a-z]{2})_[A-Z]{2}\Z/).try(:[], :country) || locale
   end
 end
