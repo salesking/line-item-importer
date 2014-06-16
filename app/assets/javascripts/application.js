754be9dc0f1e6e6a6af7cf7f8c6625abe210c15c
Georg Leciejewski <gl@salesking.eu>
2014-06-16 16:17:48 +0200

    start to refactor mapping form, tiny enhancements to mapping show/list view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
+/* load jquery, rails.js, jquery ui from bower components => jquery-ujs/src/rails.js*/
 //= require jquery
-// load rails.js from bower components => jquery-ujs/src/rails.js
 //= require jquery-ujs
-// load all in-one jquery ui 10
 //= require jquery-ui.js
 //= require fileuploader
 //= require select2
