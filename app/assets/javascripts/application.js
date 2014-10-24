dcbc10f7b2dd0232b7aced253ccd89effab6cc32
Georg Leciejewski <gl@salesking.eu>
2014-10-24 12:36:05 +0200

    update bower.json

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 /* load jquery, rails.js, jquery ui from bower components => jquery-ujs/src/rails.js*/
 //= require jquery
 //= require jquery-ujs
-//= require jquery-ui.js
 //= require fileuploader
 //= require select2
 //= require schmuugle/dist/js/schmuugle.js
